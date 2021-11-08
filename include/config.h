ed9ccd8c2313769c1e1600f21f37241c2d4cc782
Yongtao You <yongtaoy@podbean.com>
2021-11-07 19:34:12 -0800

    adjust tcp.responders.queue.size default value

--- include/config.h
+++ include/config.h
@@ -81,7 +81,7 @@
 #define CFG_TCP_RESPONDERS_PER_LISTENER         "tcp.responders.per.listener"
 #define CFG_TCP_RESPONDERS_PER_LISTENER_DEF     2
 #define CFG_TCP_RESPONDERS_QUEUE_SIZE           "tcp.responders.queue.size"
-#define CFG_TCP_RESPONDERS_QUEUE_SIZE_DEF       1024
+#define CFG_TCP_RESPONDERS_QUEUE_SIZE_DEF       128
 #define CFG_TCP_SERVER_PORT                     "tcp.server.port"
 #define CFG_TCP_SERVER_PORT_DEF                 6181
 #define CFG_TCP_SOCKET_RCVBUF_SIZE              "tcp.socket.rcvbuf.size"
