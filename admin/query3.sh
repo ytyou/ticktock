7e20224719fdbb7c464deda6f6983787638dc7e4
Yongtao You <yongtao.you@gmail.com>
2023-01-18 19:53:11 -0800

    update testing scripts

--- admin/query3.sh
+++ admin/query3.sh
@@ -6,4 +6,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 $CURL 'http://'$HOST':'$PORT'/api/query?start=1d-ago&m=avg:5m-avg:test.measurement%7Bhost=host1,_field=field2%7D'
 echo
 
+$CURL 'http://'$HOST':'$PORT'/api/query?start=1d-ago&m=avg:5m-avg:test.measurement2%7Bhost=host1,_field=field4%7D'
+echo
+
 exit 0
--- admin/write.sh
+++ admin/write.sh
@@ -4,6 +4,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $DIR/common.sh
 
 # InfluxDB's Line Protocol
-$CURL -XPOST 'http://'$HOST':'$PORT'/api/write' -d "test.measurement,host=host1,sensor=sensor1 field1=1,field2=2,field3=3"
+$CURL -XPOST 'http://'$HOST':'$PORT'/api/write' -d $'test.measurement,host=host1,sensor=sensor1 field1=1,field2=2,field3=3\ntest.measurement2,host=host1,sensor=sensor2 field4=4,field5=5'
 
 exit 0
