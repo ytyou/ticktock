d5bb6e47545d08d36cf3a6df0e384a8c66dd0349
Yongtao You <yongtao.you@gmail.com>
2023-01-19 16:07:01 -0800

    bug fixes

--- admin/query.sh
+++ admin/query.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $DIR/common.sh
 
-$CURL 'http://'$HOST':'$PORT'/api/query?start=1d-ago&m=avg:5m-avg:test.metric%7Bhost=host1%7D'
+$CURL 'http://'$HOST':'$PORT'/api/query?start=1d-ago&m=avg:test.metric%7Bhost=host1%7D'
 echo
 
 exit 0
