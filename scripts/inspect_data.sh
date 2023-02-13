3ffb3493a7096299598a5946769ca62af4326dee
Yongtao You <yongtao.you@gmail.com>
2023-02-13 10:03:39 -0800

    removed some command-line options

--- scripts/inspect_data.sh
+++ scripts/inspect_data.sh
@@ -2,7 +2,18 @@
 
 # This file is to calculate how many data points in a data dir.
 
-if [ $# -ne 1 ]; then
+# default data directory
+if [ ! -z "$TT_HOME" ]; then
+    DATA_DIR=${TT_HOME}/data
+fi
+
+# cmdline args override default
+if [ $# -eq 1 ]; then
+    DATA_DIR=$1
+fi
+
+# make sure we have a valid data directory
+if [ -z "$DATA_DIR" ]; then
     echo "Usage  : $0 <data file dir>"
     echo "Example: $0 /home/usr1/ticktock/data"
     exit 1
@@ -10,6 +21,6 @@ fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-$DIR/../bin/inspect -d $1
+$DIR/../bin/inspect -d $DATA_DIR
 
 exit 0
