fa751ba58ee6dd0174c4cbc4cdad39d12484410e
Yi <ylin30@gmail.com>
2023-08-13 14:20:43 -0700

    Use fopen/fflush instead of open/fsync

--- test/mmapTest/TestWriteFile/TestWriteFileIO.c
+++ test/mmapTest/TestWriteFile/TestWriteFileIO.c
@@ -16,8 +16,14 @@
 #include <fcntl.h>
 #include <malloc.h>
 
-int main(void) {
-  size_t pagecount = 1<<21;
+/**
+ * compile: g++ TestWriteFileIO.c
+ * run: ./a.out [fraction of 4k per write, default 1]
+ */
+int main(int argc, char** argv) {
+  int fraction = argc <=1 ? 1 : atoi(argv[1]);
+
+  size_t pagecount = 1<<22;
   size_t pagesize = getpagesize();
   printf("System page size: %zu bytes\n", pagesize);
 
@@ -26,7 +32,10 @@ int main(void) {
   //int  fd = open(file_name, O_CREAT|O_RDWR|O_DIRECT, S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
   
   // normal writes. Mem.cached will go up, mem.free down.
-  int  fd = open(file_name, O_CREAT|O_RDWR, S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
+  //int  fd = open(file_name, O_CREAT|O_RDWR, S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
+
+  FILE* file=fopen(file_name, "ab");
+  int fd = fileno(file);
 
   if (fd == -1){
         printf("Failed to open file %s", file_name);
@@ -36,7 +45,6 @@ int main(void) {
   long total_size=pagesize * pagecount - 1;
   printf("total_size=%ld\n", total_size);
  
-  int fraction=32;
   const int len = pagesize/fraction; // try 128b write each time to see how io behaves
   // For DirectIO, align mem address
   //char *tmp_str = valloc(len);
@@ -52,7 +60,7 @@ int main(void) {
   printf("tmpStr: %s\n", tmp_str);
 
   // allocate a large chunk of memory to simulate a system with memory pressure
-  size_t tmp_str_page_count = pagecount /2;
+  size_t tmp_str_page_count = pagecount / 4;
   char* tmp_str_unused = (char*)calloc(pagesize, tmp_str_page_count);
   for(int i=0; i < tmp_str_page_count; i++) {
        memcpy(tmp_str_unused + i * pagesize, tmp_str, pagesize);
@@ -64,23 +72,32 @@ int main(void) {
   for (int j=0; j < pagecount*fraction; j++) {
          //int curr_page_index = page_index[j];
 
+	 /*
          write(fd, tmp_str, len);
-
-         if ( j % 100000 ==0 ) {
-            /*
+         
+	 fsync(fd); // force fsync at each write
+	
+         if ( j % 1000000 ==0 ) {
+            
              // Force flushing.
-             if (msync(region, (unsigned)strlen(region), MS_SYNC) == -1) {
-                 perror("Could not sync the file to disk");
-             }
-             */
+             //if (msync(region, (unsigned)strlen(region), MS_SYNC) == -1) {
+             //    perror("Could not sync the file to disk");
+             //}
+             
              //printf("Len of region: %u\n", (unsigned)strlen(region));
              printf("Writes number: %u\n", j);
              fsync(fd);
-         }    
+         } 
+        */
+	
+        fwrite(tmp_str, len, 1, file);
+        fflush(file);
+
          //sleep(0.01); // sleep for 1ms
   }
 
-  close(fd);
+  //close(fd);
+  fclose(file);
   free(tmp_str);
   free(tmp_str_unused);
   return 0;
