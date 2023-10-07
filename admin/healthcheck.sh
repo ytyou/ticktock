b32fbcf3afeb739f52de3f6c15182ff92ee7ec7b
ylin30 <ylin30@gmail.com>
2023-10-07 10:04:01 -0700

    Add message to admin/healthcheck.sh

--- admin/healthcheck.sh
+++ admin/healthcheck.sh
@@ -5,11 +5,15 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $DIR/common.sh
 
+HOST=192.168.1.42
+
 RESPONSE=`$CURL -XPOST "http://$HOST:$PORT/api/admin?cmd=ping"`
 
 # exit 0 means healthy; exit 1 means unhealthy;
 if [[ $RESPONSE == "pong" ]]; then
+    echo "TickTockDB running at $HOST:$PORT"
     exit 0
 else
+    echo "TickTockDB at $HOST:$PORT no response. Possibly down!"
     exit 1
 fi
